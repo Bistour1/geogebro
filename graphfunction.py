d902a4d9a82d4a364f0810ae2e4ed4d8bb61cb9a
Bistouri <auguste.elengesa@gmail.com>
2023-10-18 17:49:43 +0200

    Add of comments in graphfunction.py

--- graphfunction.py
+++ graphfunction.py
@@ -4,13 +4,32 @@ class graphFunction(object):
         self.f = fun
 
     def getDomain(self, start: float, end: float, density: int):
+        """
+        Return the domain of the function between start and end.\n
+        the format of the domain is a list of floats:\n
+        - the first number is the start of the domain\n
+        - the second closes the domain\n
+        - the third one reopens it\n
+        - etc...\n
+        example : [4;6]U[8;13] => [4, 6, 8, 13]
+
+
+        :param start: start of the domain to check
+        :type start: float
+        :param end: end of the domain to check
+        :type end: float
+        :param density: how many numbers there are to check in the domain
+        :type density: int
+        :return: domain of the function
+        :rtype: list
+        """
         if end < start:
             end, start = start, end
         step: float = (end - start) / density
-        continuity = False
+        continuity = False  # bool saying if the area is defined by the function or not
         x = start
-        dom = []
-        for _ in range(density):
+        dom = []  # list to store result
+        for _ in range(density):  # check for each number if the function is defined at that point
             try:
                 if type(self.f(x)) != complex:
                     if not continuity:
@@ -22,4 +41,6 @@ class graphFunction(object):
                     continuity = False
             finally:
                 x += step
+        if continuity:
+            dom.append(x)
         return dom
